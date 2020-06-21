cfb14a43eb52f6669f62bed0266477282a6ad82f
Elanchiyam <elanchiyam1322000@gmail.com>
2020-06-21 23:33:17 +0530

    Restructuring folder

--- FrontEnd/client/src/components/Welcome.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class Welcome extends Component{
-    render(){
-        const {name,lastName} = this.props
-        return <h1>Hi {this.props.name}, {this.props.lastName}</h1>
-    }
-}
-export default Welcome;
